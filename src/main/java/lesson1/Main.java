2f083297a6aea795919bd80dac97771e470d567c
moliv2802 <50334418+moliv2802@users.noreply.github.com>
2019-05-09 11:03:22 +0300

    dz1 done

--- src/main/java/lesson1/Main.java
+++ src/main/java/lesson1/Main.java
@@ -1,25 +1,17 @@
 package lesson1;
 
-import lesson1.competitor.Cat;
-import lesson1.competitor.Competitor;
-import lesson1.competitor.Dog;
-import lesson1.competitor.Human;
-import lesson1.course.Cross;
-import lesson1.course.Obstacle;
-import lesson1.course.Wall;
+import lesson1.competitor.*;
+import lesson1.course.*;
 
 public class Main {
     public static void main(String[] args) {
-        Competitor[] competitors = {new Human("Боб"), new Cat("Барсик"), new Dog("Бобик")};
-        Obstacle[] course = {new Cross(80), new Wall(2), new Wall(1), new Cross(120)};
-        for (Competitor c : competitors) {
-            for (Obstacle o : course) {
-                o.doIt(c);
-                if (!c.isOnDistance()) break;
-            }
-        }
-        for (Competitor c : competitors) {
-            c.info();
-        }
+
+        Team team = new Team("Команда Игоря", new Dog("Серый"),new Dog("Тузик"), new Cat("Барсик"), new Human("Игорь"));
+        Course course = new Course(new Cross(50), new Wall(2), new Water(15), new Cross(550));
+
+        team.teamPresentation();
+        course.doIt(team);
+        team.showResults();
+
     }
 }
